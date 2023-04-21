2acf8f74f91f41aaee78d0f0c662100df16c3abd
Joan Angel Diaz Pernias <joandiaper@campus.monlau.com>
2023-04-21 20:37:53 +0200

    First

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\PersonController;
+
+Route::prefix('person')->group(function () {
+    Route::get('/',[ PersonController::class, 'getAll']);
+    Route::post('/',[ PersonController::class, 'create']);
+    Route::delete('/{id}',[ PersonController::class, 'delete']);
+    Route::get('/{id}',[ PersonController::class, 'get']);
+    Route::put('/{id}',[ PersonController::class, 'update']);
+});
